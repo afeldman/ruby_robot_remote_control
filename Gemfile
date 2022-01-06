7b254d69bc0eccf2f82e55da7a73a01eadd84c30
Anton Feldmann <anton.feldmann>
2022-01-06 11:10:11 +0100

    fanuc tube

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in ruby_robot_remote_control.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/ruby_robot_remote_control.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_robot_remote_control/version"
+
+module RubyRobotRemoteControl
+  class Error < StandardError; end
+  # Your code goes here...
+end
